b9124b27dc28972c8cd3391be94085b319d62e2e
Andreza.Pordeus <andrezapordeus@gmail.com>
2025-11-22 14:09:02 -0300

    new api key

--- config.js
+++ config.js
@@ -1,2 +1,1 @@
-// Atenção: Não recomendado expor a chave no frontend em produção!
-export const API_KEY = "AIzaSyBUGPHi63QndaMvtuoOhImhtqxTInq7e2Y"; // Coloque sua chave da API Gemini aqui
+export const API_KEY = process.env.GOOGLE_API_KEY;
