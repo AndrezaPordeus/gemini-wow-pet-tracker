eb2aa41d556117118e7c0f3ae42e94fa5e4f0db6
Andreza.Pordeus <andrezapordeus@gmail.com>
2025-11-22 13:54:35 -0300

    api key fix

--- config.js
+++ config.js
@@ -1,4 +1,2 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-export const API_KEY = process.env.GOOGLE_API_KEY;
+// Atenção: Não recomendado expor a chave no frontend em produção!
+export const API_KEY = "AIzaSyBUGPHi63QndaMvtuoOhImhtqxTInq7e2Y"; // Coloque sua chave da API Gemini aqui
